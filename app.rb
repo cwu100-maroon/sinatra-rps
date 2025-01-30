74b5fd91fe260e8bf508a8656f4c911e42e53767
cwu100-maroon <cwu100@chicagobooth.edu>
2025-01-30 06:39:30 +0000

    rock paper scissor setup

--- app.rb
+++ app.rb
@@ -7,3 +7,48 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get ("/rock") do
+  move = ["rock", "paper", "scissors"]
+  @comp_move = move.sample
+
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
+    @outcome = "lost"
+  else @comp_move == "scissors"
+    @outcome = "won"
+  end
+
+  erb(:rock)
+end
+
+get ("/paper") do
+  move = ["rock", "paper", "scissors"]
+  @comp_move = move.sample
+
+  if @comp_move == "rock"
+    @outcome = "won"
+  elsif @comp_move == "paper"
+    @outcome = "tied"
+  else @comp_move == "scissors"
+    @outcome = "lost"
+  end
+
+  erb(:paper)
+end
+
+get ("/scissors") do
+  move = ["rock", "paper", "scissors"]
+  @comp_move = move.sample
+
+  if @comp_move == "rock"
+    @outcome = "lost"
+  elsif @comp_move == "paper"
+    @outcome = "won"
+  else @comp_move == "scissors"
+    @outcome = "tied"
+  end
+
+  erb(:scissors)
+end
